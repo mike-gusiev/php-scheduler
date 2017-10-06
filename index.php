b4165da731b2a9c7895202ee1b1776a502707e5a
mikeg <mike.gusiev.a@gmail.com>
2017-10-06 18:14:33 +0300

    Schedule: refactoring 1

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once "./vendor/autoload.php";
-require_once "./app.php";
-require_once "./utils.php";
+require_once "./app/app.php";
+require_once "./app/utils.php";
 
 $json = json_decode(file_get_contents("./schedule.json"), true);
 $app = new App($json);
