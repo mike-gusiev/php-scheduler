6f2f947441fbbae005ed7f9aea6ddcf76e90d7af
mikeg <mike.gusiev.a@gmail.com>
2017-10-06 16:41:17 +0300

    Schedule: formatting of the text

--- app.php
+++ app.php
@@ -7,6 +7,16 @@ class App {
 //        echo "<pre>" . print_r($json, true) . "</pre>";
     }
 
+    function showBr($lines) {
+        echo str_repeat("<br/>\n", $lines ? $lines : 1);
+    }
+
+    function showSchedule() {
+        for ($i = 0; $i < count($this->cron); $i++) {
+            echo "<b>" . $this->cron[$i]['time'] . "</b> " . $this->cron[$i]['task'] . "<br/>\n";
+        }
+    }
+
     function showNextTasks($count) {
         echo "showNextTasks: " . $count . "<br/>\n";
         echo "Number of schedules: " . count($this->cron) . "<br/>\n";
--- index.php
+++ index.php
@@ -6,4 +6,7 @@ require "./app.php";
 $json = json_decode(file_get_contents("./schedule.json"), true);
 $app = new App($json);
 
+$app->showSchedule();
+$app->showBr(2);
+
 $app->showNextTasks(100);
